bed39dbf6388fa48475e11f0d4a098e410a41052
Hyunsoo Lee <zcarc@naver.com>
2025-06-09 14:11:06 +0900

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    css: ['~/assets/css/tailwind.css'],
+
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+
+    modules: ['shadcn-nuxt'],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    }
 })
