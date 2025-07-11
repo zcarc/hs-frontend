a2a656d56935ee28d6f0fbdd639162763161dd9b
Hyunsoo Lee <zcarc@naver.com>
2025-07-12 06:46:15 +0900

    fix: 테스트 코드 삭제

--- test/setup.ts
+++
@@ -1,5 +0,0 @@
-import { setup } from "@nuxt/test-utils/e2e";
-
-await setup({
-  // test context options
-});
--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineVitestConfig } from "@nuxt/test-utils/config";
-
-export default defineVitestConfig({
-  test: {
-    // Vitest의 다른 설정을 여기에 추가할 수 있습니다.
-    globals: true,
-    environment: "nuxt", // Nuxt 환경을 사용하도록 설정합니다.
-  },
-});
